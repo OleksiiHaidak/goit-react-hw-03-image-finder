fc0bf0aad5bc76cf9ca594ef790ec38b121655eb
OleksiiHaidak <xalexey.g@gmail.com>
2023-10-26 23:31:44 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,6 @@ import ImageGalleryItem from "components/ImageGalleryItem/ImageGalleryItem";
 import Loader from "components/Loader/Loader";
 import Modal from "components/Modal/Modal";
 import Searchbar from "components/Searchbar/Searchbar";
-import css from 'components/styles.css'
 
 // Your API key: 39445880-54b6798b916331fa60d6cbc62
 
